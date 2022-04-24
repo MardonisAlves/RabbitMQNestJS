ce109191cfd9544af9bc4e83e70e1239350f2469
Mardonis Alves <mardonisgp@gmail.com>
2022-04-24 15:44:26 -0300

    delete jogador

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,53 +1,63 @@
-import { Controller, Post, Get, Put, Res, Logger,Body, Query, Param, UsePipes, ValidationPipe } from '@nestjs/common';
-import {CriarJogadorDto} from './dtos/criar-jogador-dto';
-import {AtualizarJogadorDto} from './dtos/atualizar-jogador-dto';
-import {GetjogadorByEmail} from './dtos//getjogador-email-dto';
-import {ClienteProxySmartRank} from '../proxyrmq/cliente-proxy';
+import { Controller, Post, Get, Put, Delete, Res, Logger, Body, Query, Param, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador-dto';
+import { AtualizarJogadorDto } from './dtos/atualizar-jogador-dto';
+import { GetjogadorByEmail } from './dtos//getjogador-email-dto';
+import { ClienteProxySmartRank } from '../proxyrmq/cliente-proxy';
 import { Observable } from 'rxjs';
-import {Response} from 'express';
+import { Response } from 'express';
 @Controller('api/v1')
 export class JogadoresController {
-private  logger = new Logger(JogadoresController.name);
+	private logger = new Logger(JogadoresController.name);
 
-constructor(private ClienteProxySmartRank:ClienteProxySmartRank){}
-private  clientAdminBackend = this.ClienteProxySmartRank.getClienteProxyBackendInstance()
+	constructor(private ClienteProxySmartRank: ClienteProxySmartRank) { }
+	private clientAdminBackend = this.ClienteProxySmartRank.getClienteProxyBackendInstance()
 
-@Post('jogadores')
-@UsePipes(ValidationPipe)
-async criarjogador(@Body() criarJogadorDto:CriarJogadorDto, @Res() response:Response){
-	try{
-		const { email } = criarJogadorDto
-		const emailJogador = await this.clientAdminBackend.send('getjogador-email', {email:criarJogadorDto.email}).toPromise();
-		if(emailJogador === null){
-			const criarjogador = await this.clientAdminBackend.emit('new-jogador',criarJogadorDto);
-			return response.json({'jogador': 'Jogador  cadastrado'});
-		}else{
-			return response.json({'jogador': 'Jogador ja cadastrado'});
+	@Post('jogadores')
+	@UsePipes(ValidationPipe)
+	async criarjogador(@Body() criarJogadorDto: CriarJogadorDto, @Res() response: Response) {
+		try {
+			const { email } = criarJogadorDto
+			const emailJogador = await this.clientAdminBackend.send('getjogador-email', { email: criarJogadorDto.email }).toPromise();
+			if (emailJogador === null) {
+				const criarjogador = await this.clientAdminBackend.emit('new-jogador', criarJogadorDto);
+				return response.json({ 'jogador': 'Jogador  cadastrado' });
+			} else {
+				return response.json({ 'jogador': 'Jogador ja cadastrado' });
+			}
+		} catch (error) {
+			console.log(error)
+		}
 	}
-	}catch(error){
-		console.log(error)
+
+	@Get('jogadores')
+	getjogadores(@Query('_id') _id: string): Observable<any> {
+		return this.clientAdminBackend.send('jogadores', _id ? _id : '')
 	}
-}
 
-@Get('jogadores')
-getjogadores(@Query('_id') _id:string) :Observable<any>{
-	return  this.clientAdminBackend.send('jogadores' , _id ? _id :'')
-}
-
-@Get('jogador/email')
-@UsePipes(ValidationPipe)
-getjogadorByEmail(@Query() email:GetjogadorByEmail): Observable<any>{
+	@Get('jogador/email')
+	@UsePipes(ValidationPipe)
+	getjogadorByEmail(@Query() email: GetjogadorByEmail): Observable<any> {
 		return this.clientAdminBackend.send('getjogador-email', email)
-}
-
-@Put('jogadores/:_id')
-@UsePipes(ValidationPipe)
-atualizarJogador(@Body() atualizarJogadorDto:AtualizarJogadorDto, @Param() _id:string){
-	try{
-	
-	return this.clientAdminBackend.send('atualizar-jogador',{atualizarJogadorDto,_id})
-	}catch(error){
-		this.logger.log(error)
 	}
-}
+
+	@Put('jogadores/:_id')
+	@UsePipes(ValidationPipe)
+	atualizarJogador(@Body() atualizarJogadorDto: AtualizarJogadorDto, @Param() _id: string) {
+		try {
+
+			return this.clientAdminBackend.send('atualizar-jogador', { atualizarJogadorDto, _id })
+		} catch (error) {
+			this.logger.log(error)
+		}
+	}
+
+	@Delete('jogador/:_id')
+	deletarJogador(@Param() _id: string) {
+		try {
+			return this.clientAdminBackend.send('deletar-jogador', _id)
+		} catch (error) {
+			this.logger.log(error)
+		}
+
+	}
 }
